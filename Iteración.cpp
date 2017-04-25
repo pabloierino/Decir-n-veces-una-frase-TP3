f7eed0bf2b4ba85f465b09cdb9faaff31ac91323
Pablo Ierino <pablitoierino@hotmail.com>
2017-04-25 22:07:24 +0200

    Add files via upload

---
+++ Iteración.cpp
@@ -0,0 +1,16 @@
+/*Enviar una frase a la salida estándar muchas veces
+K1051,
+ equipo 5,
+  27 de abril 2017*/
+
+#include <iostream>
+int main ()
+{
+int x;
+for (x = 0; x <= 199; x++)
+{
+std:: cout <<"voy a programar\n";
+} 
+}
+
+
