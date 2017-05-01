0e9a18d832bae4ad83db07eac102a59ad1c4aa79
Pablo Ierino <pablitoierino@hotmail.com>
2017-05-01 11:53:28 -0300

    Add files via upload

---
+++ saltoss.cpp
@@ -0,0 +1,12 @@
+/*Enviar una frase a la salida estándar muchas veces
+K1051,
+ equipo 5,
+  27 de abril 2017*/
+#include <iostream>
+int main (){
+int i=0;
+a : std:: cout << "voy a programar\n";
+i = i + 1;
+if (i < 200) goto a;
+}
+
